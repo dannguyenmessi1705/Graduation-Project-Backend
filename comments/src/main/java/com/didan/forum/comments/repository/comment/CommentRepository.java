9753ad7bcedf4eae1fc5f8c70a658957d4c01677
dannd1 dannd1 <dannd1@viettel.com.vn>
2024-10-31 11:44:51 +0700

    Comment service: add repo

--- comments/src/main/java/com/didan/forum/comments/repository/comment/CommentRepository.java
+++ comments/src/main/java/com/didan/forum/comments/repository/comment/CommentRepository.java
@@ -4,7 +4,9 @@ import com.didan.forum.comments.entity.comment.CommentEntity;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, String> {
   Optional<CommentEntity> findCommentEntityById(String id);
@@ -14,5 +16,15 @@ public interface CommentRepository extends JpaRepository<CommentEntity, String> 
 
   @Query(value = "SELECT c FROM comments c WHERE c.postId = ?1 ORDER BY COALESCE(c.updatedAt, c.createdAt) DESC, c.content ASC")
   List<CommentEntity> findCommentByPostOrderTime(String postId);
+
+  @Transactional
+  @Modifying
+  @Query(value = "UPDATE comments c SET c.interactionScore = c.interactionScore + ?2 WHERE c.id = ?1")
+  void updateScoreComment(String commentId, Long score);
+
+  @Transactional
+  @Modifying
+  @Query(value = "DELETE FROM comments c WHERE c.postId = ?1")
+  void deleteCommentByPostId(String postId);
 
 }
