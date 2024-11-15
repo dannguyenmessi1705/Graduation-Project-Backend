a053db6eba0e25f7f87e20da89870c0c41ca4e2a
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-11-16 01:26:07 +0700

    Create config kubernetes

---
+++ eureka-server/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+w
+Gsrc/main/java/com/didan/forum/eurekaserver/EurekaServerApplication.java,6\4\64b7bb1cce399287fbf47e38b2dc57aa978aa011
--- users/src/main/java/com/didan/forum/users/UsersApplication.java
+++ users/src/main/java/com/didan/forum/users/UsersApplication.java
@@ -1,20 +1,16 @@
 package com.didan.forum.users;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
